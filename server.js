4fc3df12be0903527ccc333aa28d05c986314d06
Lucas Matheus <lucas.peres@outlook.com>
2023-10-27 11:22:57 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,72 @@
+import express from "express"
+import mysql from 'mysql'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "inventario"
+
+})
+
+app.get('/', (req, res) => {
+    const sql = "SELECT * FROM dispositivos"
+    db.query(sql, (err, result) => {
+        if (err) return res.json({ Message: "Error inside server" })
+        return res.json(result)
+    })
+})
+
+app.post("/dispositivo", (req, res) => {
+    const sql = "INSERT INTO dispositivos (`nomeDispositivo`, `nomeDono`, `modelo`, `serialKey`) VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.ownerName,
+        req.body.modelo,
+        req.body.serialKey
+    ]
+    db.query(sql, [values], (err, result) => {
+        if (err) return res.json(err)
+        return res.json(result)
+    })
+})
+
+app.get('/read/:id', (req, res) => {
+    const sql = "SELECT * FROM dispositivos WHERE id = ?"
+    const id = req.params.id
+
+    db.query(sql, [id], (err, result) => {
+        if (err) return res.json({ Message: "Error inside server" })
+        return res.json(result)
+    })
+})
+
+app.put('/update/:id', (req, res) => {
+    const sql = 'UPDATE dispositivos SET `nomeDispositivo`=?, `nomeDono`=?, `modelo`=?, `serialKey`=? WHERE id=?'
+    const id = req.params.id
+    db.query(sql, [req.body.name, req.body.ownerName,
+    req.body.modelo,
+    req.body.serialKey,
+        id], (err, result) => {
+            if (err) return res.json({ Message: "Error inside server" })
+            return res.json(result)
+        })
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const sql = "DELETE FROM dispositivos WHERE id = ?"
+    const id = req.params.id
+    db.query(sql, [id], (err, result) => {
+        if (err) return res.json({ Message: "Error inside server" })
+        return res.json(result)
+    })
+})
+
+app.listen(8081, () => {
+    console.log("Listening")
+})
